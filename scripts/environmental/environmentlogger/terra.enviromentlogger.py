94a314cd22f52d0826b26ff2100576a2beee13b9
Max Burnette <mburnet88@gmail.com>
2016-08-30 10:29:32 -0500

    Initial working version of extractor

--- scripts/environmental/environmentlogger/terra.enviromentlogger.py
+++ scripts/environmental/environmentlogger/terra.enviromentlogger.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
+
+"""
+terra.environmentlogger.py
+
+This extractor will trigger when a file is added to a dataset in Clowder. It checks if the file is an
+_environmentlogger.json file, and if so it will call EnvironmentalLoggerAnalyser.py to create a netCDF
+output file and add that to the same dataset as the original .json file.
+"""
+
 import imp
-import re
-import os
 import logging
 
 from config import *
 import pyclowder.extractors as extractors
+
 
 def main():
     global extractorName, messageType, rabbitmqExchange, rabbitmqURL, registrationEndpoints
@@ -29,6 +37,11 @@ def check_message(parameters):
                     print("skipping dataset %s, already processed" % parameters['datasetId'])
                     return False
 
+    # Only trigger extraction if the newly added file is a relevant JSON file
+    if parameters['filename'].endswith("_environmentlogger.json"):
+        return True
+    else:
+        return False
 
 def process_dataset(parameters):
     # TODO: re-enable once this is merged into Clowder: https://opensource.ncsa.illinois.edu/bitbucket/projects/CATS/repos/clowder/pull-requests/883/overview
@@ -37,18 +50,34 @@ def process_dataset(parameters):
     # if len(md) > 0:
         #extractors.remove_dataset_metadata_jsonld(parameters['host'], parameters['secretKey'], parameters['datasetId'], extractorName)
 
-    metadata = {
-        "@context": {
-            "@vocab": "https://clowder.ncsa.illinois.edu/clowder/assets/docs/api/index.html#!/files/uploadToDataset"
-        },
-        "dataset_id": parameters["datasetId"],
-        "content": {"status": "COMPLETED"},
-        "agent": {
-            "@type": "cat:extractor",
-            "extractor_id": parameters['host'] + "/api/extractors/" + extractorName
+    # Identify the relevant file among all dataset files
+    in_envlog = None
+    for f in parameters['files']:
+        if f.endswith("_environmentlogger.json"):
+            in_envlog = f
+
+    if in_envlog:
+        print(in_envlog)
+        # Execute processing on target file
+        out_netcdf = in_envlog[:-5]+".nc"
+        ela.mainProgramTrigger(in_envlog, out_netcdf)
+
+        # Send netCDF output to Clowder source dataset
+        extractors.upload_file_to_dataset(out_netcdf, parameters)
+
+        # Mark dataset as processed by this extractor
+        metadata = {
+            "@context": {
+                "@vocab": "https://clowder.ncsa.illinois.edu/clowder/assets/docs/api/index.html#!/files/uploadToDataset"
+            },
+            "dataset_id": parameters["datasetId"],
+            "content": {"status": "COMPLETED"},
+            "agent": {
+                "@type": "cat:extractor",
+                "extractor_id": parameters['host'] + "/api/extractors/" + extractorName
+            }
         }
-    }
-    extractors.upload_dataset_metadata_jsonld(mdata=metadata, parameters=parameters)
+        extractors.upload_dataset_metadata_jsonld(mdata=metadata, parameters=parameters)
 
 if __name__ == "__main__":
     global scriptPath
