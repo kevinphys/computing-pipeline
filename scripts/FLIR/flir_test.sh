b743acef1239ad0a064ad059cf2bda14f50bcc46
ZongyangLi <zongyangli86@gmail.com>
2016-10-05 15:14:32 -0500

    Update flir_test.sh

--- scripts/FLIR/flir_test.sh
+++ scripts/FLIR/flir_test.sh
@@ -1,2 +1,1 @@
-echo 'Hello World!'
-python Get_FLIR.py -i /Users/nijiang/Desktop/pythonTest/FLIR/2016-09-23 -o /Users/nijiang/Desktop/pythonTest/FLIR/output/2016-09-23
+python Get_FLIR.py -i /Users/Baker/Extractor/FLIR/2016-09-23 -o /Users/Baker/Extractor/FLIR/output/2016-09-23
